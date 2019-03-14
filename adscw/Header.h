7e2789ce5d24c09c57abecf2ba391c736eb13bed
HzKharel <43970402+HzKharel@users.noreply.github.com>
2019-03-14 12:23:57 +0000

    leaderboard implemented. Match Replays implemented.

--- adscw/Header.h
+++ adscw/Header.h
@@ -5,9 +5,14 @@ void draw_board();
 void get_player_names();
 void new_board();
 int valid_move_checker(int choice);
-void leaderboard(char name[25]);
 int computer_move();
 void pve_mode();
+void add_to_leaderboard(char name[25], int win, int loss);
+void print_leaderboard();
+int get_free_replay_index();
+void add_to_replay(struct game_moves moves[9]);
+void match_replays_selection();
+void launch_replay(int game_index);
 
 struct game_moves {
 	int board_move;
@@ -18,5 +23,11 @@ struct game_moves {
 struct Leaderboard {
 	int wins;
 	int losses;
+	int total;
 	char name[25];
+};
+
+struct Match
+{
+	struct game_moves match_moves[9];
 };
--- adscw/main.c
+++ adscw/main.c
@@ -1,12 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include<stdlib.h> 
 #include <string.h>
 #include "Header.h"
 
 
 //global variables
-char player1[25], player2[25], board[9];
+char player1[25], player2[25], board[10];
+struct Leaderboard leaderboard_array[100];
+struct Match match_replays[100];
+
 
 
 //main entry point for the game
@@ -40,13 +44,13 @@ void  main(void)
 		pve_mode();
 		break;
 	case 3:
-		printf("Launch Replays");
+		match_replays_selection();
 		break;
 	case 4:
-		printf("Launch scoreboard");
+		print_leaderboard();
 		break;
 	case 5:
-		leaderboard(name);
+		printf("Exit.\n");
 		break;
 	default:
 		printf("Invalid Selection.");
@@ -210,9 +214,19 @@ void pvp_mode()
 			int win = check_win();
 			if (win == 1)
 			{
+				
 				system("cls");
 				draw_board();
 				printf("%s won the game!", &player);
+				if (strcmp(player, player1) == 0) {
+					add_to_leaderboard(player1, 1, 0);
+					add_to_leaderboard(player2, 0, 1);
+				}
+				else
+				{
+					add_to_leaderboard(player2, 1, 0);
+					add_to_leaderboard(player1, 0, 1);
+				}
 				break;
 
 			}
@@ -221,6 +235,8 @@ void pvp_mode()
 				system("cls");
 				draw_board();
 				printf("Draw!");
+				add_to_leaderboard(player2, 0, 0);
+				add_to_leaderboard(player2, 0, 0);
 				break;
 
 			}
@@ -242,7 +258,7 @@ void pvp_mode()
 		}
 
 		//if the user undo's the last move
-		if (valid_move == 0)
+		else if (valid_move == 0)
 		{
 			if (moves_index > 0)
 			{
@@ -255,7 +271,7 @@ void pvp_mode()
 				printf("\nNo moves left to redo!\n");
 			}
 		}
-		if (valid_move == 11)
+		else if (valid_move == 11)
 		{
 			char temp_mark = moves[moves_index].mark;
 			if(temp_mark == 'X' || temp_mark == 'O')
@@ -270,15 +286,37 @@ void pvp_mode()
 			}
 		}
 		else {
+			valid_move = valid_move;
 			printf("Invalid Move!");
 			getch();
 		}
+	
+	}
 
+	add_to_replay(moves);
+}
+int get_free_replay_index() 
+{
+	for (int i = 0; i < 100; i++)
+	{
+		if (strcmp(match_replays[i].match_moves[1].player_name, "")==0)
+		{
+			return i;
+		}
+	}
 
-
+}
+void add_to_replay(struct game_moves moves[9])
+{
+	int match_index = get_free_replay_index();
+	
+	for (int i = 0; i < 9; i++) 
+	{
+		match_replays[match_index].match_moves[i].board_move = moves[i].board_move;
+		match_replays[match_index].match_moves[i].mark = moves[i].mark;
+		strcpy(match_replays[match_index].match_moves[i].player_name, moves[i].player_name);
 	}
 }
-
 
 void pve_mode()
 {
@@ -379,18 +417,105 @@ int computer_move()
 		}
 	}
 }
-void leaderboard(char name[25]) 
+void add_to_leaderboard(char name[25], int win, int loss) 
 {
-	FILE* File_Editor;
-	File_Editor = fopen("leaderboard.txt", "r");
-	char line[100];
-	if (!File_Editor == NULL) 
+	int exists = 0;
+	for (int idx = 0; idx < 100; idx++)
 	{
-		while (fgets(line, sizeof(line), File_Editor)) {
-			
+		if ((strcmp(leaderboard_array[idx].name, name) == 0))
+		{
+			if (win == 1)
+			{
+				leaderboard_array[idx].wins++;
+			}
+			if (loss == 1)
+			{
+				leaderboard_array[idx].losses++;
+			}
+
+			leaderboard_array[idx].total++;
+			exists = 1;
 		}
 	}
+	if (exists == 0) 
+	{
+		for (int idx = 0; idx < 100; idx++)
+		{
+			if ((strcmp(leaderboard_array[idx].name, "") == 0))
+			{
+				 strcpy(leaderboard_array[idx].name, name);
+				 leaderboard_array[idx].wins = 0;
+				 leaderboard_array[idx].losses = 0;
+				 leaderboard_array[idx].total = 0;
 
+				 if (win == 1)
+				 {
+					 leaderboard_array[idx].wins++;
+				 }
+				 if (loss == 1)
+				 {
+					 leaderboard_array[idx].losses++;
+				 }
+				 leaderboard_array[idx].total++;
+				 break;
 
+			}
+		}
+
+	}
+
+}
+
+void print_leaderboard()
+{
+	system("cls");
+	printf("Name \t\t Wins \t Losses \t Total\n\n");
+	for (int i = 0; i < 100; i++) {
+		if (strcmp(leaderboard_array[i].name, "")==1) {
+			printf("%s \t\t %d \t %d \t\t %d\n", leaderboard_array[i].name, leaderboard_array[i].wins, leaderboard_array[i].losses, leaderboard_array[i].total);
+		}
+	}
+	getch();
+}
+
+void match_replays_selection()
+{
+	system("cls");
+	printf("Match Replays  \n\n");
+	for (int i = 0; i < 100; i++)
+	{
+		if ((strcmp(match_replays[i].match_moves[0].player_name, "") == 1) && (strcmp(match_replays[i].match_moves[1].player_name, "") == 1))
+		{
+			printf("%d ) Match between %s and %s\n", i, match_replays[i].match_moves[0].player_name, match_replays[i].match_moves[1].player_name);
+		}
+	}
+	int match_idx = 0;
+	scanf("%d", &match_idx);;
+	launch_replay(match_idx);
+}
+
+void launch_replay(int game_index) 
+{
+	char name1[25];
+	char name2[25];
+	strcpy(name1, match_replays[game_index].match_moves[1].player_name);
+	strcpy(name2, match_replays[game_index].match_moves[2].player_name);
+	system("cls");
+	new_board();
+	draw_board();
+	printf("Match Replay between %s and %s",name1,name2);
+	for (int i = 0; i < 9; i++)
+	{
+		getch();
+		int temp_move = match_replays[game_index].match_moves[i].board_move;
+		char temp_mark = match_replays[game_index].match_moves[i].mark;
+
+		if (temp_mark == 'X' || temp_mark == 'O')
+		{
+			board[temp_move] = temp_mark;
+		}
+		draw_board();
+		
+	}
 
 }
